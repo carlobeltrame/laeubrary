ee312ef7e997635d1bfd5e16d50ebc647d687b2e
Carlo Beltrame <carlo@beltra.me>
2021-05-27 23:40:27 +0200

    Add favicon

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Styles -->
